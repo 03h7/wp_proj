0e4eb29ed6197a5986b33036207c0d92ba9844f6
Davenese <sajous.davenese@gmail.com>
2023-03-21 17:04:42 +0100

    je commit

---
+++ src/components/Home.js
@@ -0,0 +1,40 @@
+import {useEffect, useState} from "react";
+import axios from 'axios';
+
+export const Home = () => {
+
+    const [data, setData] = useState([])
+    useEffect(() => {
+        axios.get('http://g4.esiee-it.o3creative.fr/wp-json/wp/v2/posts').then((response) => {
+            console.log(data)
+            setData(response.data[0])
+
+        })
+    }, [])
+
+    return (
+
+        <div className="App">
+            <header className="">
+
+                <title>{data.title.rendered}</title>
+                <p>{data.title.rendered}
+                </p>
+                <a
+                    className="App-link"
+                    href={data.guid.rendered}
+                    target="_blank"
+                    rel="noopener noreferrer"
+                >
+                    Page 1
+                </a>
+            </header>
+
+            <body>
+            <p>{data.content.rendered}</p>
+            </body>
+
+        </div>
+
+    )
+}
---
+++ src/components/Quiz.js
@@ -0,0 +1,28 @@
+import {useEffect, useState} from "react";
+import axios from "axios";
+
+export const Quiz = (navigation) => {
+    const [dataQuiz, setDataQuiz] = useState([])
+
+    useEffect(() => {
+        axios.get(`http://g4.esiee-it.o3creative.fr/wp-json/wp/v2/quiz/23`).then((response) => {
+            setDataQuiz(response.data)
+
+        })
+    }, [])
+
+
+    return (
+        <div>
+
+            <div> {dataQuiz.acf.question}</div>
+            {dataQuiz.acf.reponses.map((value) => <button
+                key={value.reponse} value={value.reponse}>{value.reponse}</button>)}
+
+
+            <button onClick={() => navigation.navigate(Quiz, {number: 14})}>Suivant</button>
+
+        </div>
+
+    )
+}
