01ace45edd772cbd966438bd5ccdf636821fca2a
Davenese <sajous.davenese@gmail.com>
2023-03-24 16:16:34 +0100

    refix

--- src/components/Quiz.js
+++ src/components/Quiz.js
@@ -2,10 +2,7 @@ import {useEffect, useState} from "react";
 import axios from "axios";
 import {NavLink, useLocation, useParams} from "react-router-dom";
 import '../styles/Quiz.css'
-import pho from '../assets/pho.jpg'
 import Countdown from "./Countdown";
-import '../styles/Quiz.css'
-import food from '../assets/pho.jpg'
 
 
 export const Quiz = () => {
@@ -21,7 +18,6 @@ export const Quiz = () => {
     }
 
     const handleCheckboxChange = (event) => {
-
         setCheckedItem({
             id: parseInt(event.target.name), answer: (event.target.value)
         });
@@ -41,35 +37,35 @@ export const Quiz = () => {
             {/* <img className="img" src={pho}></img> */}
             <h1>Question {dataQuiz.acf.numquestionpage}</h1>
 
-        <div className="currQuest">
-            <p className="question"> {dataQuiz.acf.question}</p>
-            <Countdown />
+            <div className="currQuest">
+                <p className="question"> {dataQuiz.acf.question}</p>
+                <Countdown/>
 
-            <div className="allAnswers">
-            {dataQuiz.acf.reponses.map(item => (
-                <label key={item.reponse}>
-                    <input
-                        className="answer"
-                        type="checkbox"
-                        name={item.id}
-                        value={item.id}
-                        checked={checkedItem === item.id}
-                        onChange={handleCheckboxChange}
-                    />
-                    <span className="labelText">{item.reponse}</span>
-                    <br></br>
-                </label>)
-            )}
-            </div>
-            <div className="nextButton">
-                {
-                    !dataQuiz.acf.numeropage ? (
-                        <NavLink to={`/result/${nbGoodAnswers.length + 1}`}>Résultats</NavLink>) : (
-                        <NavLink to={`/quiz/${dataQuiz.acf.numeropage}`}
-                                 onClick={() => addItem()}>Question
-                            suivante</NavLink>)
-                }
-            </div>
+                <div className="allAnswers">
+                    {dataQuiz.acf.reponses.map(item => (
+                        <label key={item.reponse}>
+                            <input
+                                className="answer"
+                                type="checkbox"
+                                name={item.id}
+                                value={item.id}
+                                checked={checkedItem.id === item.id}
+                                onChange={handleCheckboxChange}
+                            />
+                            <span className="labelText">{item.reponse}</span>
+                            <br></br>
+                        </label>)
+                    )}
+                </div>
+                <div className="nextButton">
+                    {
+                        !dataQuiz.acf.numeropage ? (
+                            <NavLink to={`/result/${nbGoodAnswers.length + 1}`}>Résultats</NavLink>) : (
+                            <NavLink to={`/quiz/${dataQuiz.acf.numeropage}`}
+                                     onClick={() => addItem()}>Question
+                                suivante</NavLink>)
+                    }
+                </div>
             </div>
         </div>
     )
