915df42e4967cc0fbcc2fe74fe046a914bb36b01
Davenese <sajous.davenese@gmail.com>
2023-03-24 15:55:17 +0100

    refix

--- src/components/Home.js
+++ src/components/Home.js
@@ -15,28 +15,27 @@ export const Home = () => {
 
     return (
         <div className="App">
-            <header className="">
-                <img className="logoaccueil" src="https://images.ctfassets.net/gi6fitfj7iu4/5pPJ6ZLR5irhfpobU6DQlW/689df14ded419613f908e21c700d5878/LOGO_BEAN.png?q=80&w=800"></img>
-                
+            <header className="headerClass">
+                <img className="logoaccueil"
+                     src="https://images.ctfassets.net/gi6fitfj7iu4/5pPJ6ZLR5irhfpobU6DQlW/689df14ded419613f908e21c700d5878/LOGO_BEAN.png?q=80&w=800"></img>
+
                 <ul>
                     <li><h1><NavLink to="/quiz/14">Quizz </NavLink></h1></li>
                     <li><h1><NavLink to="page/51">Articles </NavLink></h1></li>
                 </ul>
             </header>
             <div>
-            <p className="desc"><h2>{dataPages.acf.description}</h2></p>
+                <p className="desc"><h2>{dataPages.acf.description}</h2></p>
                 <p className="corps">{dataPages.acf.corps}</p>
                 <br></br>
-              <div>
-              
-                {dataPages.acf.image.map((item, index) => (
-              <img className={`img_${index}`} key={index} src={item} alt="img" height="350vh" width="80%"></img>))}
-              </div>
+                <div>
+
+                    {dataPages.acf.image.map((item, index) => (
+                        <img className={`img_${index}`} key={index} src={item} alt="img" height="350vh"
+                             width="80%"></img>))}
+                </div>
             </div>
 
-        
-
-           
 
         </div>
     )
